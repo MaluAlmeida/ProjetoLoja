debb50185d4e0f7b2dc3860e9448acbe6ed0b9fe
MaluAlmeida <malugoncalves2707@gmail.com>
2025-08-20 08:44:07 -0300

    Autorizando as seções

--- ProjetoLoja/Program.cs
+++ ProjetoLoja/Program.cs
@@ -5,6 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// 1. REGISTRAR OS SERVIÇOS DE SESSÃO
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Tempo de inatividade da sessão (pode ajustar)
+    options.Cookie.HttpOnly = true; // Impede que o cookie seja acessado por scripts do lado do cliente
+    options.Cookie.IsEssential = true; // Torna o cookie de sessão essencial para a funcionalidade do aplicativo
+});
+
+// REGISTRAR A CONNECTION STRING COMO UM SERVIÇO STRING AQUI
+builder.Services.AddSingleton<string>(builder.Configuration.GetConnectionString("DefaultConnection")!);
+
 // Registrar Repositórios
 // injetar os repositorios
 builder.Services.AddScoped<ProdutoRepositorio>();
@@ -22,7 +33,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
+// ESTA LINHA É CRUCIAL E DEVE VIR ANTES DE app.UseAuthorization() ou app.MapControllerRoute() <<<
+app.UseSession(); //permite a sessão antes de autorizar
+
+app.UseAuthorization(); //altoriza a navegação
 
 app.MapControllerRoute(
     name: "default",
